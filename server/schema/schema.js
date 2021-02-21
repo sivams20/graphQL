051afe18df9127d556d76a28b2cb91a840ec6415
sivams20 <sivams20@gmail.com>
2021-02-21 13:18:08 +0530

    Removed commented code

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,21 +9,6 @@ const { GraphQLObjectType,
     GraphQLID,
     GraphQLList } = graphql;
 
-// dummy data
-// var books = [
-//     { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
-//     { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
-//     { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
-//     { name: 'Monk Who Sold His Ferrari', genre: 'Fiction', id: '4', authorId: '1' },
-//     { name: 'Wise and otherwise', genre: 'Real life stories', id: '5', authorId: '2' }
-// ];
-
-// var authors = [
-//     { name: 'Patrick Rothfuss', age: 44, id: '1' },
-//     { name: 'Brandon Sanderson', age: 42, id: '2' },
-//     { name: 'Terry Pratchett', age: 66, id: '3' }
-// ];
-
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
