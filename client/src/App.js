aa939a28947437312462d60d9ea09de5e9998680
sivams20 <sivams20@gmail.com>
2021-03-05 08:53:37 +0530

    Setup apollo client, created form

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import { Component } from "react";
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 //Components
 import BookList from './components/BookList';
+import AddBook from './components/AddBook';
+
+//apolloclient set up
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App" >
-        <h1>Introduction to GraphQL</h1>
-        <BookList />
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main" >
+          <h1>Introduction to GraphQL</h1>
+          <BookList />
+          <AddBook />
+        </div>
+      </ApolloProvider>
     );
   }
 }
--- server/app.js
+++ server/app.js
@@ -3,8 +3,12 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+//allow cross origin request
+app.use(cors());
 
 //connecting to mongodb atlas
 mongoose.connect('mongodb+srv://siv_admin:sivam@cluster0.6hhfs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
